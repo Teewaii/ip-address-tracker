ec82a9960b696f48165c58aaa2ea4b5cd07f1ad0
Taiye Ajimati <taiyeajimati@gmail.com>
2022-09-12 14:55:59 +0400

    First office commit

---
+++ ip-address-tracker/src/components/main/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Main.css'
+export default function Main() {
+  return (
+   <div className="topBanner">
+    <h1 className="header">IP Addres</h1>
+   </div>
+  )
+}
