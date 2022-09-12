0bd8a3860d8dd57f37f021a920953d90c1f439ff
Taiye Ajimati <taiyeajimati@gmail.com>
2022-09-12 18:50:54 +0400

    Last office commit

--- ip-address-tracker/src/components/main/Main.jsx
+++ ip-address-tracker/src/components/main/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Main.css'
+import bkg from '../../images/pattern-bg.png'
 export default function Main() {
   return (
    <div className="topBanner">
+<img src={bkg} alt="" />
     <h1 className="header">IP Addres</h1>
    </div>
   )
